6d3b9704b80d2eddf62cdfcc491cd6eec9f37d2b
Ciprian Carausu <carausuciprian@gmail.com>
2019-09-28 11:35:41 +0300

    Mask username/password.

--- src/com/cc/Main.java
+++ src/com/cc/Main.java
@@ -1,18 +1,33 @@
 package com.cc;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class Main {
 
     public static void main(String[] args) {
+        String user = "";
+        String password = "";
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         try {
-            Connection connection; // used to open a connection to a database
-            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", "root", "1995");
-            System.out.println("Connection successfully");
+            System.out.println("Please enter the username: ");
+            user = reader.readLine();
+
+            System.out.println("Please enter the password: ");
+            password = reader.readLine();
         } catch (Exception ex) {
+        }
+
+        try {
+            Connection connection;
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306", user, password);
+            System.out.println("Connection successfully");
+        } catch (
+                Exception ex) {
             System.out.println(ex.getMessage());
-            for (StackTraceElement entry : ex.getStackTrace()){
+            for (StackTraceElement entry : ex.getStackTrace()) {
                 System.out.println(entry.getMethodName());
             }
         }
